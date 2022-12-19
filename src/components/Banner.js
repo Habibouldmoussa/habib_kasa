eb796c7b023dcbe1dd4fdd30e0facf63a8e95acf
Habib OULDMOUSSA <omhabibal@gmail.com>
2022-12-19 10:11:40 +0100

    uninstall styled-components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,11 @@
-//Importation des composants react
 import Styles from './Banner.module.css'
-import styled from 'styled-components'
-import imageHome from '../assets/bannerHome.png'
-import imageAbout from '../assets/bannerApropos.png'
-//On récupere le type de backround du banner et en integre l'image qui corespend a la page 
-const Styledbanner = styled.section`
-background: url(${props => props.background === 'bannerHome' ? imageHome : imageAbout}) no-repeat center ; 
-border-radius: 30px;
-`
 
 function Banner(props) {
     return (
-        <Styledbanner background={props.background} className={Styles.banner}>
+        <section  className={`${Styles.banner} ${ props.background === "bannerHome" ? Styles.bannerHome  : Styles.bannerAbout}`} >
             <div className={Styles.banner__filter}></div>
             <span className={Styles.banner__text}>{props.text}</span>
-        </Styledbanner>
+        </section>
     )
 }
 export default Banner
